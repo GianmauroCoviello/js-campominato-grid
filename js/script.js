547f4ac77ccf50f89c11d576b0cc7972eb7ee450
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-28 23:44:56 +0200

    [DOCS] Aggiunta commenti.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// FUNZIONE CHE CREA IL QUADRATINO ALL'INTERNO DEL DOM
 function createSquare() {
     let square = document.createElement('div')
     square.classList.add('square')
@@ -5,21 +7,26 @@ function createSquare() {
 }
 
 console.log(createSquare())
-
+// VARIABILE CHE INSERISCA LA GRIGLIA ALL' INTERNO DEL DOM
 let grid = document.getElementById('grid')
 
-
+// CICLO FOR INSERITO PER CICLARE I QUADRATINI
 for (i =0; i < 100 ; i++) {
-
+    
+    // VARIABILE CHE RIPRENDE LA FUNZIONE 
     let square = createSquare()
+    // VARIABILE CHE INNIETTA GLI INDICI NUMERICI DEL CICLO ALL'INTERNO DEI QUADRATINI
     square.innerText = i + 1
 
+    // ADDEVENTLISTENER ALLA CLASSE SQUARE PER FAR CAMBIARE E RIMUOVERE IL COLORE AI QUADRATINI
     square.addEventListener('click', function(){
+        // FUNZIONI CHE AGGIUNGONO E TOLGONO LA CLASSE PER IL COLORE AI QUADRATINI('SQUARE') 
         this.classList.toggle('click-color')
-        
+        // console.log per riuscire a dare il numero esatto del quadratino in cui ci troviamo
+        console.log(this.innerText)
         
     })
-
+    // APPEND ALLA GRIGLIA PER INSERIRE AL SUO INTERNO LA CLASSE SQUARE CHE SAREBBERO I QUADRATINI
     grid.append(square)
     
     
