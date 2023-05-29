c1f43b5c0780c6f152fc5a62aa6a209bc7af8f1c
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-29 10:54:56 +0200

    [DOCS] Piccole modifiche effettuate.

--- js/script.js
+++ js/script.js
@@ -8,26 +8,34 @@ function createSquare() {
 
 console.log(createSquare())
 // VARIABILE CHE INSERISCA LA GRIGLIA ALL' INTERNO DEL DOM
-let grid = document.getElementById('grid')
+let button = document.getElementById('riproduci')
 
-// CICLO FOR INSERITO PER CICLARE I QUADRATINI
-for (i =0; i < 100 ; i++) {
-    
-    // VARIABILE CHE RIPRENDE LA FUNZIONE 
-    let square = createSquare()
-    // VARIABILE CHE INNIETTA GLI INDICI NUMERICI DEL CICLO ALL'INTERNO DEI QUADRATINI
-    square.innerText = i + 1
+// EVENTLISTENER AL BUTTON PER RIPRODURRE AL SUO CLICK LA GRIGLIA CON I QUADRATINI
+button.addEventListener('click', function() {
 
-    // ADDEVENTLISTENER ALLA CLASSE SQUARE PER FAR CAMBIARE E RIMUOVERE IL COLORE AI QUADRATINI
-    square.addEventListener('click', function(){
-        // FUNZIONI CHE AGGIUNGONO E TOLGONO LA CLASSE PER IL COLORE AI QUADRATINI('SQUARE') 
-        this.classList.toggle('click-color')
-        // console.log per riuscire a dare il numero esatto del quadratino in cui ci troviamo
-        console.log(this.innerText)
+    let grid = document.getElementById('grid')
+
+    // CICLO FOR INSERITO PER CICLARE I QUADRATINI
+    for (i =0; i < 100 ; i++) {
         
-    })
-    // APPEND ALLA GRIGLIA PER INSERIRE AL SUO INTERNO LA CLASSE SQUARE CHE SAREBBERO I QUADRATINI
-    grid.append(square)
-    
-    
+        // VARIABILE CHE RIPRENDE LA FUNZIONE 
+        let square = createSquare()
+        // VARIABILE CHE INNIETTA GLI INDICI NUMERICI DEL CICLO ALL'INTERNO DEI QUADRATINI
+        square.innerText = i + 1
+
+        // ADDEVENTLISTENER ALLA CLASSE SQUARE PER FAR CAMBIARE E RIMUOVERE IL COLORE AI QUADRATINI
+        square.addEventListener('click', function(){
+            // FUNZIONI CHE AGGIUNGONO E TOLGONO LA CLASSE PER IL COLORE AI QUADRATINI('SQUARE') 
+            this.classList.toggle('click-color')
+            // console.log per riuscire a dare il numero esatto del quadratino in cui ci troviamo
+            console.log(this.innerText)
+            
+        })
+        // APPEND ALLA GRIGLIA PER INSERIRE AL SUO INTERNO LA CLASSE SQUARE CHE SAREBBERO I QUADRATINI
+        grid.append(square)
+        
+        
 }
+    
+})
+
